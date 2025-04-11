db14f076605ac9825acb0472a3f256d730ae6984
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-04-11 16:11:25 +0530

    basic done in linkedlist

--- start.js
+++ start.js
@@ -52,35 +52,76 @@
 
 
 // // Inserting new node to the end //
+// function node(data) {
+//     this.data = data;
+//     this.next = null;
+// }
+// function insertNode(newNode, head) { // go to the last node through current variable
+//     let current = head;
+//     while (current.next != null) {
+//         current = current.next;
+//     }
+//     current.next = newNode;
+// }
+
+// function traversal(head) { // traverse to the entire linkedList and print the data of all nodes, the newNode has inserted to the last as tail successfully 
+//     let current = head;
+//     while (current != null) {
+//         console.log(current.data);
+//         current = current.next;
+//     }
+// }
+
+
+// let head = new node(12)
+// let mid = new node(18);
+// let tail = new node(24);
+// head.next = mid;
+// mid.next = tail;
+// let newNode = new node(30) //node which has to be inserted to the last of linkedList
+
+// insertNode(newNode, head);
+// traversal( head);
+
+
+
+
+// Inserting new node to a particular index //
 function node(data) {
     this.data = data;
     this.next = null;
 }
-function insertNode(newNode, head) {
+
+let head = new node(13) //creating the nodeds
+let m1 = new node(26)
+let m2 = new node(39)
+let tail = new node(52)
+
+head.next = m1; // placing the nodes
+m1.next = m2;
+m2.next = tail;
+
+let newNode = new node("JavaScript");
+
+function insertIdx(head, newNode, target) { //function to insert the newNode node to the index
     let current = head;
-    while (current.next != null) {
+    let idx = 0;
+    while (current != null && idx != target - 1) {
         current = current.next;
+        idx++;
     }
+    let temp = current.next;
     current.next = newNode;
+    newNode.next = temp;
 }
 
-function traversal(head) {
+function traverse(head) {
     let current = head;
-    // console.log(current)
     while (current != null) {
-        console.log(current.data);
+        console.log(current.data)
         current = current.next;
-        // console.log("object")
     }
 }
 
-
-let head = new node(12)
-let mid = new node(18);
-let tail = new node(24);
-head.next = mid;
-mid.next = tail;
-let newNode = new node(30)
-
-insertNode(newNode, head);
-traversal( head);
+insertIdx(head, newNode, 2);
+traverse(head);
