1fb2e974b0b8ce76d1a4283efc7bdfeec8ad03f0
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-04-12 10:15:34 +0530

    added one file name as asLast in Deletion folder

--- basic.js
+++ basic.js
@@ -22,3 +22,40 @@ function printList(node) {
 
 // Print the Linked List
 printList(head);
+
+
+
+
+//  creating a node //
+// function node(data) {
+//     this.data = data;
+//     this.next = null;
+// }
+
+// let head = new node(5)
+// console.log(head);
+
+
+
+
+
+// // creating multiple nodes //
+// function node(data) {
+//     this.data = data;
+//     this.next = null;
+// }
+
+// let head = new node(5)
+// let mid = new node(10);
+// head.next = mid;
+// let tail = new node(20);
+// mid.next = tail;
+// console.log(head)
+
+// // [5,next --> [10,next --> [20,next --> Null]]]
+
+
+
+
+
+
