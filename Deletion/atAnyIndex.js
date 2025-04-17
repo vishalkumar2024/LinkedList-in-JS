3862a3b2df464060263429aaf60bb63ce24e988f
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-04-17 20:21:31 +0530

    modify

--- Deletion/atAnyIndex.js
+++ Deletion/atAnyIndex.js
@@ -1,3 +1,23 @@
+
+function deleteNode(head, target) {
+    let current = head;
+    let idx = 0;
+    while (current.next != null && idx != target - 1) {
+        current = current.next;
+        idx++
+    }
+    current.next = current.next.next;
+
+    current = head;
+    let res="";
+    while (current != null) {
+        res += current.data + " ";
+        current = current.next;
+    }
+    return res;
+
+}
+
 function Node(data) {
     this.data = data;
     this.next = null
@@ -11,32 +31,10 @@ let m4 = new Node(1);
 
 
 head.next = m1
-m1.next = m2; 
-m2.next = m3; 
-m3.next = m4; 
+m1.next = m2;
+m2.next = m3;
+m3.next = m4;
 m4.next = null;
 
 
-function deleteNode(head, target) {
-    let current = head;
-    let idx = 0;
-    while (current.next != null && idx != target - 1) {
-        current = current.next;
-        idx++
-    }
-    current.next = current.next.next;
-    return head;
-}
-
-
-function traversal(head) {
-    let current = head;
-    while (current != null) {
-        console.log(current.data);
-        current = current.next;
-    }
-}
-
 console.log(deleteNode(head, 1));
-traversal(head);
-
