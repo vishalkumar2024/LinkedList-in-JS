edc0ca081156af0c8d8d67bde25d0b1b683e25de
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-04-20 09:41:01 +0530

    modified

--- insertion/atEnd.js
+++ insertion/atEnd.js
@@ -1,25 +1,28 @@
 
 //  Inserting new node to the end  //
-function node(data) {
-    this.data = data;
-    this.next = null;
-}
-function insertNode(newNode, head) { // go to the last node through current variable
+
+function insertion(head, newNode) {
+    // go to the last node through current variable
     let current = head;
     while (current.next != null) {
         current = current.next;
     }
     current.next = newNode;
-}
 
-function traversal(head) { // traverse to the entire linkedList and print the data of all nodes, the newNode has inserted to the last as tail successfully 
-    let current = head;
+    // traverse to the entire linkedList and print the data of all nodes, the newNode has inserted to the last as tail successfully 
+    current = head;
+    let res = "";
     while (current != null) {
-        console.log(current.data);
+        res += current.data + " ";
         current = current.next;
     }
+    return res;
 }
 
+function node(data) {
+    this.data = data;
+    this.next = null;
+}
 
 let head = new node(12)
 let mid = new node(18);
@@ -28,6 +31,5 @@ head.next = mid;
 mid.next = tail;
 let newNode = new node("JavaScript") //node which has to be inserted to the last of linkedList
 
-insertNode(newNode, head);
-traversal( head);
+console.log(insertion(head, newNode))
 
