47a32fae7fc3a6e30d84f0be934def41e407bb00
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-04-22 21:48:53 +0530

    modify insertion folder

--- insertion/atAnyIndex.js
+++ insertion/atAnyIndex.js
@@ -1,4 +1,23 @@
 
+function insertIdx(head, newNode, target) { //function to insert the newNode node to the index
+    let current = head;
+    let idx = 0;
+    while (current != null && idx != target - 1) {
+        current = current.next;
+        idx++;
+    }
+    let temp = current.next; //Storing the address of next of current nodes 
+    current.next = newNode; //newNode holding the info from left side 
+    newNode.next = temp;   //newNode  holding the info from right side
+
+    current = head;
+    let res = "";
+    while (current != null) {
+        res += current.data + " ";
+        current = current.next;
+    }
+    return res;
+}
 // Inserting new node to a particular index //
 function node(data) {
     this.data = data;
@@ -13,28 +32,6 @@ let tail = new node(52)
 head.next = m1; // placing the nodes
 m1.next = m2;
 m2.next = tail;
-
 let newNode = new node("JavaScript");
 
-function insertIdx(head, newNode, target) { //function to insert the newNode node to the index
-    let current = head;
-    let idx = 0;
-    while (current != null && idx != target - 1) {
-        current = current.next;
-        idx++;
-    }
-    let temp = current.next; //Storing the address of next of current nodes 
-    current.next = newNode; //newNode holding the info from left side 
-    newNode.next = temp;   //newNode  holding the info from right side
-}
-
-function traverse(head) {
-    let current = head;
-    while (current != null) {
-        console.log(current.data)
-        current = current.next;
-    }
-}
-
-insertIdx(head, newNode, 2);
-traverse(head);
+console.log(insertIdx(head, newNode, 2));
