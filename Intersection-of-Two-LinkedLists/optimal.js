f44debb64706f787f86f3263ab16ef029b16d0f0
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-05-01 11:23:02 +0530

    added one file name as optimal.js  in in intersection of two linkedlist

--- Intersection-of-Two-LinkedLists/optimal.js
+++ Intersection-of-Two-LinkedLists/optimal.js
@@ -1,70 +1,41 @@
-//Leetcode 160
-// function getIntersectionNode(head1, head2) {
-//     if (!head1 || !head2) return null
-//     let d1 = head1;
-//     let d2 = head2;
-
-//     // if (d1 == null || d2 == null) {
-//     //     return null;
-//     // }
-
-//     while (d1 !== d2 ) {
-//         d1 = d1 ? d1.next : head2;
-//         d2 = d2 ? d2.next : head1;
-//     }
-//     return d1;
-
-
-// }
+// leetcode 160
 
 function getIntersectionNode(headA, headB) {
-    if (!headA || !headB) return null;
-    
     let a = headA;
     let b = headB;
 
     while (a !== b) {
-        a = a ? a.next : headB;
-        b = b ? b.next : headA;
+        a = a === null ? headB : a.next;
+        b = b === null ? headA : b.next;
     }
 
-    return a; // or b
-};
+    return a; // returns the intersecting node or null
+}
 
 function Node(data) {
     this.data = data;
     this.next = null;
 }
-
 let head1 = new Node(4);
 let m1 = new Node(1);
-let m2 = new Node(8);
+let inter = new Node(8); // Intersection node
 let m3 = new Node(4);
 let m4 = new Node(5);
 
 head1.next = m1;
-m1.next = m2;
-m2.next = m3;
+m1.next = inter;
+inter.next = m3;
 m3.next = m4;
-m4.next = null;
-
-
 
 let head2 = new Node(5);
 let n1 = new Node(6);
 let n2 = new Node(1);
-let n3 = new Node(5);
-let n4 = new Node(4);
-let n5 = new Node(5);
 
 head2.next = n1;
 n1.next = n2;
-n2.next = n3;
-n3.next = n4;
-n4.next = n5;
-n5.next = null;
+n2.next = inter; 
 
-console.log(getIntersectionNode(headA, headB));
+console.log(getIntersectionNode(head1, head2));
 
-// TC = O(m+n) + O(m)
-// SC = O(m+n)
+// TC = O(m+n)
+// SC = O(1)
