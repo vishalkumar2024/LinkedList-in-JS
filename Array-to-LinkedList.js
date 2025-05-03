6fcc249e51de5992095eb80f1b921b678f3b10ec
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-05-03 09:34:04 +0530

    added a file name as array  to list .js

---
+++ Array-to-LinkedList.js
@@ -0,0 +1,24 @@
+// Given an array of integer arr. Your task is to construct the linked list from 
+// arr & return the head of the linked list.
+
+function arrayToList(arr) {
+    let head = new Node(arr[0]);
+    let ptr = head;
+    for (let i = 1; i < arr.length; i++) {
+        let n = new Node(arr[i])
+        ptr.next = n;
+        ptr = ptr.next;
+    }
+    ptr.next = null;
+    return head;
+}
+
+function Node(data) {
+    this.data = data;
+    this.next = null
+}
+
+let arr = [1, 2, 3, 4, 5]
+
+console.log(arrayToList(arr));
+
