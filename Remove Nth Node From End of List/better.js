3cd6f80f22fd4be256b6232a43596426d7c8ba02
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-05-06 14:36:47 +0530

    added a file name as better in it

---
+++ Remove Nth Node From End of List/better.js
@@ -0,0 +1,54 @@
+//Leetcode 19
+
+function removeNthFromEnd(head, n) {
+
+    let curr = head;
+    count = 0
+    while (curr) {  //TC=O(n)
+        curr = curr.next
+        count++;
+    }
+    let diff = count - n;
+
+    if (diff == 0) return head.next;
+
+    curr = head;
+    idx = 1
+    while (idx < diff) { //TC=O(n)
+        curr = curr.next;
+        idx++;
+    }
+
+    curr.next = curr.next.next;
+
+    // return head;
+
+    curr = head
+    while (curr) {
+        console.log(curr.data)
+        curr = curr.next
+    }
+
+}
+
+function Node(data) {
+    this.data = data;
+    this.next = null
+}
+
+let head = new Node(1);
+let m1 = new Node(2);
+let m2 = new Node(3);
+let m3 = new Node(4);
+let m4 = new Node(5);
+
+head.next = m1;
+m1.next = m2;
+m2.next = m3;
+m3.next = m4;
+m4.next = null;
+
+console.log(removeNthFromEnd(head, 2));
+
+// TC=O(2n)
+// SC=O(1)
